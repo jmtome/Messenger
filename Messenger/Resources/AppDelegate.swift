39d718edcc3a1918e14d9b265f97af6507004f99
Juan Manuel Tome <jtome@fi.uba.ar>
2020-10-23 00:42:48 -0300

    Fixes

--- Messenger/Resources/AppDelegate.swift
+++ Messenger/Resources/AppDelegate.swift
@@ -18,34 +18,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         FirebaseApp.configure()
-
+        
         ApplicationDelegate.shared.application(
             application,
             didFinishLaunchingWithOptions: launchOptions
         )
         GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
         GIDSignIn.sharedInstance()?.delegate = self
-
+        
         return true
     }
-          
+    
     func application(
         _ app: UIApplication,
         open url: URL,
         options: [UIApplication.OpenURLOptionsKey : Any] = [:]
     ) -> Bool {
-
+        
         ApplicationDelegate.shared.application(
             app,
             open: url,
             sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
             annotation: options[UIApplication.OpenURLOptionsKey.annotation]
         )
-
+        
         return GIDSignIn.sharedInstance().handle(url)
-
+        
     }
-
+    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         guard error == nil else {
             if let error = error {
@@ -56,17 +56,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         
         
         guard let email = user.profile.email,
-           let firstName = user.profile.givenName,
-           let lastName = user.profile.familyName else { return }
+              let firstName = user.profile.givenName,
+              let lastName = user.profile.familyName else { return }
         
         print("Did log in with google user :\(firstName), \(lastName), email: \(email)")
-
+        
         
         DatabaseManager.shared.userExists(with: email) { (exists) in
             if !exists {
                 //insert to database
                 let chatUser = ChatAppUser(firstName: firstName, lastName: lastName,  emailAddress: email)
-
+                
                 DatabaseManager.shared.insertUser(with: chatUser, completion: { success in
                     if success {
                         
@@ -101,7 +101,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
                         
                         
                         
-                       
+                        
                     }
                 })
             }
@@ -131,7 +131,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         print("Google user was disconected")
     }
     
-  
+    
     
 }
 
